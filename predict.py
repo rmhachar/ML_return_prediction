e876a7b9d72d04863c7eaa67fe56f6e91c4323fd
rmhachar <richard.hachar@gmail.com>
2018-03-17 14:01:07 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -57,16 +57,16 @@ features
 # Factorize the training set
 y = pd.factorize(train['ten_percent_or_greater'])[0]
 
-# STEP 4:
+# STEP 4: Create and train the classifier
 
-# Create a random forest Classifier. By convention, clf means 'Classifier'
+# Create a random forest classifier. By convention, clf means 'classifier'
 clf = RandomForestClassifier(n_jobs=2, random_state=0)
 
-# Train the Classifier to take the training features and learn how they relate
+# Train the classifier to take the training features and learn how they relate
 # to the training y (whether or not a stock's annual return is ten percent or greater)
 clf.fit(train[features], y)
 
-# Apply the Classifier we trained to the test data (which, remember, it has never seen before)
+# Apply the classifier we trained to the test data
 clf.predict(test[features])[1:10]
 
 # Create an array of predictions
@@ -78,5 +78,18 @@ predictions = list(predict_temp.flatten())
 # Add the list to data_temp under a new column titled 'predictions'
 test_data_temp = test
 test_data_temp['predictions'] = predictions
+
+# STEP 5: Create a confusion matrix to observe results
+
+# View the predicted probabilities of the first 10 observations
+clf.predict_proba(test[features])[0:10]
+
+possible_y = np.array(['down','up'])
+
+### Create actual english names for the plants for each predicted plant class
+preds = possible_y[clf.predict(test[features])]
+
+# Create confusion matrix
+pd.crosstab(test['ten_percent_or_greater'], preds, rownames=['y'], colnames=['possible_y'])
 
 
