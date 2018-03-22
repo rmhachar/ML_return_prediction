1e2d280814a0b47008eded8681318faf7e590d29
rmhachar <richard.hachar@gmail.com>
2018-03-21 21:23:29 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+
 # STEP 1: Setup
 
 # Import packages
@@ -5,6 +6,9 @@ import re
 import glob
 import numpy as np
 import pandas as pd
+import sklearn
+
+# Import Sci-Kit Learn
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
@@ -16,7 +20,7 @@ from sklearn.ensemble import RandomForestClassifier
 np.random.seed(0)
 
 # Load File
-file_loc = "data/market_return_data_full_sample.csv"
+file_loc = "market_return_data_full_sample.csv"
 
 # Read File
 data = pd.read_csv(file_loc)
@@ -26,24 +30,7 @@ df = pd.DataFrame(data,columns=data.columns)
 
 # STEP 2: Split DF into training set and testing set randomly
 
-# Randomly assign training column
-# A 1 is part of the training set, a 0 is part of the test set
-df['training'] = np.random.uniform(0,1,len(df)) <= .75
-
 data_filtered = df.drop(['Count','Company'],axis=1)
-
-# Create the training and testing sets from data_filtered
-
-train = data_filtered[data_filtered['training']==True]
-test = data_filtered[data_filtered['training']==False]
-
-# Print the amount of observations in each set
-
-print('Number of observations in the training data:', len(train))
-
-print('Number of observations in the test data:',len(test))
-
-# STEP 3:
 
 # Create a list of the feature column's names
 features = df.drop(['Count','Company'],axis=1)
@@ -55,41 +42,53 @@ features = features.columns[:11]
 features
 
 # Factorize the training set
-y = pd.factorize(train['ten_percent_or_greater'])[0]
+y = pd.factorize(data_filtered['ten_percent_or_greater'])[0]
+x = data_filtered
 
-# STEP 4: Create and train the classifier
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=.25, random_state =0)
 
-# Create a random forest classifier. By convention, clf means 'classifier'
+print('Number of observations in the training data:', len(x_train))
+
+print('Number of observations in the test data:',len(x_test))
+
+data_filtered
+
+# STEP 3: Create and Train the Classifier
+
+# Create a random forest Classifier. By convention, clf means 'Classifier'
 clf = RandomForestClassifier(n_jobs=2, random_state=0)
 
-# Train the classifier to take the training features and learn how they relate
+# Train the Classifier to take the training features and learn how they relate
 # to the training y (whether or not a stock's annual return is ten percent or greater)
-clf.fit(train[features], y)
+clf.fit(x_train[features], y_train)
 
-# Apply the classifier we trained to the test data
-clf.predict(test[features])[1:10]
+# STEP 4: Predict using the Classifier
+
+# Apply the Classifier we trained to the test data (which, remember, it has never seen before)
+clf.predict(x_test[features])[1:10]
 
 # Create an array of predictions
-predict_temp = clf.predict(test[features])
+predict_temp = clf.predict(x_test[features])
 
 # Create a list of predictions from the array
 predictions = list(predict_temp.flatten())
 
 # Add the list to data_temp under a new column titled 'predictions'
-test_data_temp = test
+test_data_temp = x_test
 test_data_temp['predictions'] = predictions
 
-# STEP 5: Create a confusion matrix to observe results
+# STEP 5: Create a Confusion Matrix
 
 # View the predicted probabilities of the first 10 observations
-clf.predict_proba(test[features])[0:10]
+clf.predict_proba(x_test[features])[0:10]
 
 possible_y = np.array(['down','up'])
 
 ### Create actual english names for the plants for each predicted plant class
-preds = possible_y[clf.predict(test[features])]
+preds = possible_y[clf.predict(x_test[features])]
 
 # Create confusion matrix
-pd.crosstab(test['ten_percent_or_greater'], preds, rownames=['y'], colnames=['possible_y'])
+pd.crosstab(y_test, preds, rownames=['y'], colnames=['possible_y'])
 
 
